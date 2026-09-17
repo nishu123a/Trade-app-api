bc7ab85ba2f7dc079c8f8210ecc0f2b431ad8276
Nishu Sinha <sinhanishu1610@gmail.com>
2026-09-17 11:23:14 +0000

    Refactored pom files post folder refactoring

---
+++ config-server/src/main/java/in/nishusinha/tradeapp/config_server/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package in.nishusinha.tradeapp.config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
